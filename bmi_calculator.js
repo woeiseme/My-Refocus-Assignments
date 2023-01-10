2f56b6a0b1a37c5bb4238800f6650ee17c5acfc8
Kenji Aggarao <kenji@Kenjis-MacBook-Pro-2.local>
2023-01-10 18:45:37 +0800

    Manual Testing for BMI Calculator

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,4 +1,4 @@
-function bmiCalculator(weight, height) {
+/*function bmiCalculator(weight, height) {
     var bmi = weight / (height * height);
     if (bmi < 18.5) {
         return "Underweight, BMI: "+bmi.toFixed(1);
@@ -10,5 +10,39 @@ function bmiCalculator(weight, height) {
         return "Obesity, BMI: "+bmi.toFixed(1);
     }
 }
+*/
+function bmiCalculator(weight, height) {
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+        return "Invalid input";
+    } else {
+        var bmi = weight / (height * height);
+        if (bmi < 18.5) {
+            return "Underweight, BMI: "+bmi.toFixed(1);
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+            return "Normal weight, BMI: "+bmi.toFixed(1);
+        } else if (bmi >= 25 && bmi <= 29.9) {
+            return "Overweight, BMI: "+bmi.toFixed(1);
+        } else {
+            return "Obesity, BMI: "+bmi.toFixed(1);
+        }
+    }
+}
+
+
 
 console.log(bmiCalculator(71, 1.80)); 
+
+
+function testBMI(){
+    const result = bmiCalculator(71, 1.80);
+    const expected = 21.9;
+    
+    
+    
+    console.assert(
+    result === expected,
+    `The result ${result} doesn't match the expected value ${expected}`
+    )
+}
+
+console.log(testBMI())
